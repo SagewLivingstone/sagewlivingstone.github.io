f5098bfaff5ae3aed3801604761f412b6b74d8de
Sage <SageWLivingstone@gmail.com>
2024-10-09 13:17:36 -0700

    try using tailwind nuxt module instead

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    }
-  }
-})
+
+  modules: ["@nuxtjs/tailwindcss"],
+});
